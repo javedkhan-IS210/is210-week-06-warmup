91a7e4136f41f71cbe7459abdc3276b0723dc974
javedkhan-IS210 <javed.khan@spsmail.cuny.edu>
2016-10-02 22:06:46 +0000

    created task_01.py and added a list and a tuple

---
+++ task_01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Staticly defining a list and a tuple using standard constructors"""
+
+ELEMENTS = (None, 'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron',
+            'Carbon')
+
+OPERATIONS_ORDER = ['Parentheses', 'Exponents', 'Multiplication', 'Division',
+                    'Addition', 'Subtraction']
